918236b5d3780c51f7cfc5913b8337befd19d068
yorikcv <yorik.cv@gmail.com>
2014-10-01 15:28:55 +0300

    no coments

---
+++ public/js/Views/LoginAndRegistration/LoginUserView.js
@@ -0,0 +1,54 @@
+define('LoginUserView', ['jquery', 'underscore', 'backbone', 'jqueryjson', 'LoginUserModel', 'RegistrationUserModel',
+        'RegistrationUserView', 'text!loginTemplate'
+    ],
+    function($, _, Backbone, jqJSON, LoginUserModel, RegistrationUserModel, RegistrationUserView, loginTemplate) {
+        var LoginUserView = Backbone.View.extend({
+
+            selectors: {
+                userLoginButton: '#submitButton',
+                userRegisterButton: '#registerButton',
+                loginForm: '.b_loginForm'
+            },
+
+            loginFormContainer: $('body'),
+
+            template: _.template(loginTemplate),
+
+            /* PRIVATE METHODS */
+
+            _attachEvents: function() {
+                $(this.selectors.userLoginButton).on('click', $.proxy(this._loginUser, this));
+                $(this.selectors.userRegisterButton).on('click', $.proxy(this._registerUser, this));
+            },
+
+            _loginUser: function() {
+                /*You can write here everything you need for login*/
+                var formData = ($(this.selectors.loginForm).serializeJSON());
+
+                var loginModel = new LoginUserModel();
+                loginModel.set(formData);
+
+                loginModel.save();
+                return false;
+            },
+
+            /*
+             * Open registration form
+             */
+            _registerUser: function() {
+                new RegistrationUserView({
+                    model: new RegistrationUserModel
+                }).render();
+            },
+
+            /*PUBLIC METHODS*/
+
+            render: function() {
+                this.loginFormContainer.html(this.template());
+                this._attachEvents();
+                return this;
+            }
+        });
+
+        return LoginUserView;
+    });
