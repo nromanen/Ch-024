d809c7e1b615a26682d27199fed69305fc60f1d1
yorikcv <yorik.cv@gmail.com>
2014-11-10 02:03:56 +0200

    improve settings

--- public/js/Views/LoginAndRegistration/SettingsUserView.js
+++ public/js/Views/LoginAndRegistration/SettingsUserView.js
@@ -2,26 +2,20 @@ define('SettingsUserView', [
     'jquery',
     'underscore',
     'backbone',
+    'ControllerView',
     'text',
     'text!settingsTemplate'
 ], function(
     $,
     _,
     Backbone,
+    ControllerView,
     text,
     settingsTemplate) {
 
     var SettingsUserView = Backbone.View.extend({
 
         template: _.template(settingsTemplate),
-
-        selectors: {
-            changeSurname: "#changeSurname",
-            changePhone: "#changePhone",
-            currentPassword: "#currentPassword",
-            changePassword: "#changePassword",
-            repeatPassword: "#repeatPassword",
-        },
 
         initialize: function(options) {
             this.$el.html(this.template());
@@ -30,6 +24,12 @@ define('SettingsUserView', [
             this.passwordModel = options.passwordModel;
             this.profileModel.on("invalid", $.proxy(this._defineErrorProfile, this));
             this.passwordModel.on("invalid", $.proxy(this._defineErrorPassword, this));
+            this._addProfileValuesInInputs();
+
+            this.$("#changePhone").mask("+99(999)999-99-99", {
+                placeholder: "_"
+            });
+
         },
 
         _attachEvents: function() {
@@ -38,15 +38,15 @@ define('SettingsUserView', [
         },
 
         _keyPressEvent: function() {
+            var that = this
             $('html').keypress(jQuery.proxy(function(event) {
                 if (event.keyCode === 13) {
-                    this.$("#saveProfileButton").click();
+                    that.$(".tab-content .active .btn").click();
                 }
             }));
         },
 
         _defineErrorProfile: function(model, errors) {
-            // console.log(model);
             $('.groupProfile .errors').html('');
             $('.groupProfile input').removeClass('borderRed');
             _.each(errors, function(error) {
@@ -56,8 +56,19 @@ define('SettingsUserView', [
             }, this);
         },
 
+        _addProfileValuesInInputs: function() {
+            var that = this;
+
+            this.profileModel.fetch({
+                type: 'GET',
+                success: function() {
+                    that.setProfileInput(that.profileModel.toJSON());
+                }
+            });
+
+        },
+
         _defineErrorPassword: function(model, errors) {
-            // console.log(model);
             $('.groupPassword .errors').html('');
             $('.groupPassword input').removeClass('borderRed');
             _.each(errors, function(error) {
@@ -67,28 +78,79 @@ define('SettingsUserView', [
             }, this);
         },
 
+        clearProfileInput: function() {
+            this.$('#changeName').val("");
+            this.$('#changeSurname').val("");
+            this.$('#changePhone').val("");
+        },
+
+        setProfileInput: function(modelObject) {
+            this.$('#changeName').val(modelObject.username);
+            this.$('#changeSurname').val(modelObject.surname);
+            this.$('#changePhone').val(modelObject.phone);
+        },
+
         _checkFormProfile: function() {
             var dataProfile = {};
 
-            dataProfile.changeName = $('#changeName').val();
-            dataProfile.changeSurname = $(this.selectors.changeSurname).val();
-            dataProfile.changePhone = $(this.selectors.changePhone).val();
+            dataProfile.changeName = this.$('#changeName').val();
+            dataProfile.changeSurname = this.$('#changeSurname').val();
+            dataProfile.changePhone = this.$('#changePhone').val();
 
             this.profileModel.set(dataProfile, {
                 validate: true
             });
+
+            if (!this.profileModel.validationError) {
+                this.profileModel.save(null, {
+                    type: 'PUT',
+                    statusCode: {
+                        201: function(model, response) {
+                            ControllerView.showAlertSuccess({
+                                message: "Your profile has changed"
+                            });
+                        }
+                    }
+                });
+            }
+        },
+
+        clearPasswordInput: function() {
+            this.$('#currentPassword').val("");
+            this.$('#changePassword').val("");
+            this.$('#repeatPassword').val("");
         },
 
         _checkFormPassword: function() {
-            var dataPassword = {};
+            var dataPassword = {},
+                that = this;
 
-            dataPassword.currentPassword = $(this.selectors.currentPassword).val();
-            dataPassword.changePassword = $(this.selectors.changePassword).val();
-            dataPassword.repeatPassword = $(this.selectors.repeatPassword).val();
+            dataPassword.currentPassword = this.$('#currentPassword').val();
+            dataPassword.changePassword = this.$('#changePassword').val();
+            dataPassword.repeatPassword = this.$('#repeatPassword').val();
 
             this.passwordModel.set(dataPassword, {
                 validate: true
             });
+            if (!this.passwordModel.validationError) {
+                this.passwordModel.save(null, {
+                    type: 'PUT',
+                    statusCode: {
+                        201: function(model, response) {
+                            ControllerView.showAlertSuccess({
+                                message: "Your password have changed"
+                            });
+                            that.clearPasswordInput();
+                        },
+                        401: function(model, response) {
+                            ControllerView.showAlertError({
+                                message: "Current password is wrong"
+                            });
+                            that.clearPasswordInput();
+                        }
+                    }
+                });
+            };
         },
 
         render: function() {
